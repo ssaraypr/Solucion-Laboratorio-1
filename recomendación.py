b9a28f74a8af976bbdb46bde6cee1e15976afb4b
Saray Pacheco Ramos <sarapache10@gmail.com>
2024-11-05 12:15:22 -0500

    actualizar funcion

--- recomendación.py
+++ recomendación.py
@@ -8,37 +8,33 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
-@app.get("/recomendacion/{titulo}")
-
-def recomendacion (Titulo: str):
+def obtener_recomendacion (dataset, Titulo):
     '''
     '''
-    #Acceder al archivo de Movies
-    movies = pd.read_csv("Datasets/movies_recomendación.csv")
-          
+            
     #Verificar que las columnas Title/genre_1_name/original_language sean tipo string
-    movies["title"] = movies["title"].astype("string")
-    movies["genre_1_name"] = movies["genre_1_name"].astype("string")
+    dataset["title"] = dataset["title"].astype("string")
+    dataset["genre_1_name"] = dataset["genre_1_name"].astype("string")
     
     # Se convierte las columnas title/genero(genre_1_name)/original_language a una representación numérica usando TF-IDF
     vectorizar = TfidfVectorizer()
-    title_tf = vectorizar.fit_transform(movies["title"])
-    genero_tf = vectorizar.fit_transform(movies["genre_1_name"])
+    title_tf = vectorizar.fit_transform(dataset["title"])
+    genero_tf = vectorizar.fit_transform(dataset["genre_1_name"])
             
     #Se añade las columnas númericas a la matriz de características
     #Se usaran las variables vote_average/vote_count/popularity/genero
     #Característias para el sistema de recomendación
-    features = np.column_stack([title_tf.toarray(), genero_tf.toarray(), movies["vote_average"], movies["vote_count"], movies["popularity"]])
+    features = np.column_stack([title_tf.toarray(), genero_tf.toarray(), dataset["vote_average"], dataset["vote_count"], dataset["popularity"]])
     
     #Se reindexa el index
-    movies = movies.reset_index(drop=True)
+    dataset = dataset.reset_index(drop=True)
 
     # Se calcula la matriz de similitud de coseno
     similarity_matrix = cosine_similarity(features)
 
     #Se filtra el dataset por la película buscada
     Titulo = Titulo.lower().strip()
-    pelicula = movies[movies["title"].str.lower() == Titulo]
+    pelicula = dataset[dataset["title"].str.lower() == Titulo]
 
     if pelicula.empty:
         return "Película No encontrada"
@@ -49,7 +45,23 @@ def recomendacion (Titulo: str):
     peliculas_mas_sim = np.argsort(-peliculas_similares)
 
     #Se obtiene la lista de las 5 películas más similares
-    top_5 = movies.loc[peliculas_mas_sim[1:6], "title"]
+    top_5 = dataset.loc[peliculas_mas_sim[1:6], "title"]
 
     resultado = {"Las 5 películas más similares a": Titulo, "son": top_5.reset_index().to_dict(orient="records")}
-    return JSONResponse(content=jsonable_encoder(resultado), media_type="application/json")
+    return resultado
+
+@app.get("/recomendacion/{titulo}", response_model= dict)
+
+async def recomendacion (Titulo: str):
+    '''
+    '''
+    try:
+        movies = pd.read_csv(r"C:\Users\SARAY\Documents\Cursos Cortos\Henry\Laboratorios Individual\Laboratorio 1\Solucion Laboratorio 1\Datasets\movies_recomendación.csv")
+
+
+        #Aplicar función
+        resultado = obtener_recomendacion(movies, Titulo)
+        return JSONResponse(content=jsonable_encoder(resultado), media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al leer el archivo: {str(e)}")
+    
